c9bd4d610824bf3e08ab00c88fc0b4063400bb69
Samuelson Okoi <samuelsonokoi@gmail.com>
2021-03-02 22:25:51 +0100

    Added default project styles and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    CartComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
